e6f3950dc346eb1090e70626a49c91eb4f702ef6
Laniakea <grwgithub@gmail.com>
2019-08-05 12:45:05 -0300

    ( chore ) -> App Config
    Configuration for Webpack and Babel on the project.
    
    Create alias to access app fodlers

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  entry: ["babel-polyfill", "./src/index.js"],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+         'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+};
