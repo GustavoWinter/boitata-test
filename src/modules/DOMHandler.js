8770f149ec403e9098010190198bcc6eef93b9c6
Laniakea <grwgithub@gmail.com>
2019-08-08 14:53:33 -0300

    ( docs_

--- src/modules/DOMHandler.js
+++ src/modules/DOMHandler.js
@@ -7,10 +7,15 @@ export default class DOMHandler {
     parent.appendChild(el)
   }
 
+  // Append all element to the parent
   static appendAll(parent, elements) {
     elements.forEach(el => parent.appendChild(el))
   }
 
+  /*
+    Clone an element (original) , and append the children (element) in it
+    add all classes to the clone
+  */
   static cloneToEncapsulate(original, element, cloneStyle = []) {
     const clone = original.cloneNode()
     clone.classList.add(...cloneStyle)
@@ -18,6 +23,7 @@ export default class DOMHandler {
     return clone
   }
 
+  /* Append all elemts to the same container that has been cloned */
   static cloneAndEncapsulateAll(container, elements = [], styles = []) {
     return elements.map((element, index) => DOMHandler.cloneToEncapsulate(container, element, styles[index]))
   }
